d5a1ec7c6d5409e66a469c35ea627abd967b658f
joaocosta21 <joaocostared@gmail.com>
2023-08-28 01:16:27 +0100

    comment 600 users on a post not tested

--- main.py
+++ main.py
@@ -7,4 +7,11 @@ cl = Client()
 cl.login(config.username, config.password)
 
 media_id = cl.media_id(cl.media_pk_from_url('https://www.instagram.com/p/CwSU0U4MZ4M/'))
-comment = cl.media_comment(media_id, "@")
+omment = cl.media_comment(media_id, "@" + username)
+media = cl.user_info_by_username('koala.pt').dict()
+accs = list(media.values())[0]
+followers = cl.user_followers(accs).keys()
+x = range(600)
+for i in x:
+    username = cl.username_from_user_id(followers[i])
+    comment = cl.media_comment(media_id, "@" + username)
