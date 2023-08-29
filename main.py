6c9cdad9077575e477a4eeafa54b8325d17a7ac9
joaocosta21 <joaocostared@gmail.com>
2023-08-29 03:58:34 +0100

    updated to fix a relog bug

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from instagrapi import Client
-from instagrapi.types import Usertag, Location
 import config
-
 
 cl = Client()
 cl.login(config.username, config.password)
 
-media_id = cl.media_id(cl.media_pk_from_url('https://www.instagram.com/p/CwSU0U4MZ4M/'))
+media_id = cl.media_id(cl.media_pk_from_url('https://www.instagram.com/p/BXyGGl8BhoK/'))
 media = cl.user_info_by_username('koala.pt').dict()
 accs = list(media.values())[0]
-followers = cl.user_followers(accs).keys()
-x = range(600)
-for i in x:
-    username = cl.username_from_user_id(followers[i])
-    comment = cl.media_comment(media_id, "@" + username)
+followers = list(cl.user_followers(accs))
+# x = range(600)
+# for i in x:
+username = cl.username_from_user_id(followers[1])
+print(username)
+try:
+    bot_account = cl.account_info()
+except:
+    cl.relogin()
+comment = cl.media_comment(media_id, "@" + username)
